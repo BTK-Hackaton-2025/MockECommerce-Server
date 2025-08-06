0d56e316e645f407e0e1331889732df4a957a847
altugmalkan <altug33@windowslive.com>
2025-08-06 19:31:55 +0300

    fix: order mapping bug

--- MockECommerce.BusinessLayer/Mapping/OrderMapping.cs
+++ MockECommerce.BusinessLayer/Mapping/OrderMapping.cs
@@ -17,13 +17,15 @@ public class OrderMapping : Profile
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.OrderDate, opt => opt.Ignore())
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => "Pending"))
-            .ForMember(dest => dest.Product, opt => opt.Ignore());
+            .ForMember(dest => dest.Product, opt => opt.Ignore())
+            .ForMember(dest => dest.Customer, opt => opt.Ignore());
 
         // UpdateOrderDto to Order Entity
         CreateMap<UpdateOrderDto, Order>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.ProductId, opt => opt.Ignore())
             .ForMember(dest => dest.Product, opt => opt.Ignore())
+            .ForMember(dest => dest.Customer, opt => opt.Ignore())
             .ForMember(dest => dest.CustomerId, opt => opt.Ignore())
             .ForMember(dest => dest.OrderDate, opt => opt.Ignore());
     }
